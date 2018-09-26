c74b4f48607600581d11bd5374f76eab49083ac2
Xiang <XiangCZ929@gmail.com>
2018-09-26 23:24:38 +0800

    Update middlewares.py

--- wangyiyunSpider/middlewares.py
+++ wangyiyunSpider/middlewares.py
@@ -9,6 +9,7 @@ import json
 import logging
 from scrapy import signals
 import requests
+import random
 
 
 class ProxyMiddleware():
@@ -25,7 +26,7 @@ class ProxyMiddleware():
                 return proxy
         except requests.ConnectionError:
             return False
-          
+
     def process_request(self, request, spider):
         # if request.meta.get('retry_times'):
         if len(self.proxy):
@@ -34,11 +35,12 @@ class ProxyMiddleware():
             request.meta['proxy'] = uri
             self.proxy = ''
             
+
     def process_response(self, request, response, spider):
         if 'Cheating' in response.text:
             self.proxy = self.get_random_proxy()
             return request
-        return response       
+        return response
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -46,3 +48,14 @@ class ProxyMiddleware():
         return cls(
             proxy_url=settings.get('PROXY_URL')
         )
+
+class RandomUserAgentMiddleware():
+	def __init__(self, agents):
+		self.agents = agents
+
+	@classmethod
+	def from_crawler(cls, crawler):
+		return cls(crawler.settings.getlist('USER_AGENTS'))
+
+	def process_request(self, request, spider):
+		request.headers.setdefault('User-Agent', random.choice(self.agents))
